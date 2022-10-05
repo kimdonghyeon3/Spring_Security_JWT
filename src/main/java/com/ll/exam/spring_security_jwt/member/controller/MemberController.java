059ece64263b82febe5c7e7ce9212e4bb67d9bb4
kimdonghyeon3 <sh2115521@naver.com>
2022-10-05 15:37:45 +0900

    feat : response entity 도입

--- src/main/java/com/ll/exam/spring_security_jwt/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/spring_security_jwt/member/controller/MemberController.java
@@ -2,6 +2,9 @@ package com.ll.exam.spring_security_jwt.member.controller;
 
 
 import com.ll.exam.spring_security_jwt.member.dto.LoginDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberController {
 
     @RequestMapping("/member/login")
-    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response){
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto, HttpServletResponse response){
 
         response.addHeader("Authentication","myToken");
 
-        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "token");
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 }
