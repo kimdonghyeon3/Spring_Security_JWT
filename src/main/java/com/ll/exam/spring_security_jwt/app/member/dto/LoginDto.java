0dbbf02e23ab129f69c9a9b01792c29a60135468
kimdonghyeon3 <sh2115521@naver.com>
2022-10-07 16:09:14 +0900

    feat : spring valid 도입

--- src/main/java/com/ll/exam/spring_security_jwt/app/member/dto/LoginDto.java
+++ src/main/java/com/ll/exam/spring_security_jwt/app/member/dto/LoginDto.java
@@ -2,9 +2,13 @@ package com.ll.exam.spring_security_jwt.app.member.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class LoginDto {
+    @NotEmpty(message = "username 을(를) 입력해주세요.")
     private String username;
+    @NotEmpty(message = "password 을(를) 입력해주세요.")
     private String password;
 
     public boolean isNotValid() {
